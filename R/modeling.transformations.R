3b806865dd770e083e655f76f5cc4fd3df2843b9
Paul Hively <pbhively@gmail.com>
2016-10-12 17:14:14 -0500

    Modeling methodology

--- R/modeling.transformations.R
+++ R/modeling.transformations.R
@@ -7,6 +7,16 @@ full.data <- read.csv("data/ABE Modeling.csv", stringsAsFactors=FALSE, strip.whi
 source("R/clean.data.R")
 
 # Drop unneeded variables
-mdat <- dat %>% select(
-  one_of(scan("results/mdat.vars.txt", what="character"))
-)
+mdat <- dat %>% 
+  select(
+    # Must be one of the variables identified by Boruta
+    one_of(scan("results/mdat.vars.txt", what="character"))
+  ) %>%
+  # Numeric transformations
+  mutate(
+    Research.Non.Capacity = Research.Non.Capacity >= 1
+  ) %>%
+  # Factor transformations
+  mutate(
+    Giving.First.Trans.Amt = Giving.First.Trans.Amt %>% cut(breaks=c(0, 25, 50, 100, Inf), include.lowest=T, right=F)
+  )
